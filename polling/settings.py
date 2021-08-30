55d8d592ca8b1457e92a704e58ebb74387faccec
arzWZM <arzWZM@protonmail.com>
2021-08-30 14:38:14 +0530

    Updated settings.py to include secret key temporarily

--- polling/settings.py
+++ polling/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+#SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = iql)an966qsxsg&8onv&^s4k3-g&4i(0+ddu491za5r!%%!*fi
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
