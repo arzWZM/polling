4493f40f408b2618a3b3b13a33672dbf88d7de8c
arzWZM <arzWZM@protonmail.com>
2021-08-30 14:39:00 +0530

    Updated settings.py to include secret key temporarily

--- polling/settings.py
+++ polling/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = iql)an966qsxsg&8onv&^s4k3-g&4i(0+ddu491za5r!%%!*fi
+SECRET_KEY = "iql)an966qsxsg&8onv&^s4k3-g&4i(0+ddu491za5r!%%!*fi"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
